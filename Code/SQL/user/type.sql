2c2a9d6d9bc5daa588dd8de4ed5721986a7ab96f
Chen, Yi-Wen <b01a01207@ntu.edu.tw>
2020-06-04 12:45:16 +0800

    Create type.sql

---
+++ Code/SQL/user/type.sql
@@ -0,0 +1,35 @@
+SELECT *,
+        CASE WHEN start_week = '2015-05-03' AND quit_week = '2016-11-20' THEN "WHOLE"
+            WHEN start_week > '2015-05-03' AND quit_week = '2016-11-20' THEN "NEW"
+            WHEN start_week = '2015-05-03' AND quit_week < '2016-11-20' THEN "QUIT"
+            ELSE "NEW_QUIT" END AS TYPE
+FROM
+(SELECT user_id,
+        COUNT(*) AS total_week,
+        MIN(Week) AS start_week,
+        MAX(Week) AS quit_week
+FROM
+(SELECT user_id,
+        Week
+FROM
+(SELECT user_id,
+        Week,
+        COUNT(*) AS week_total
+FROM(
+(SELECT user_id,
+        post_created_date_CT,
+        DATE_ADD(DATE(2015,5,3), INTERVAL CAST(FLOOR(DATE_DIFF(CAST(post_created_date_CT AS DATE), DATE('2015-05-03'), DAY)/7) AS INT64) WEEK) AS Week
+FROM `USdata.1000_page_us_user_like_post_201501_to_201611_all`
+WHERE TIMESTAMP(post_created_date_CT) >= TIMESTAMP('2015-05-03')
+AND TIMESTAMP(post_created_date_CT) < TIMESTAMP('2016-11-27'))
+UNION DISTINCT
+(SELECT user_id,
+        post_created_date_CT,
+        DATE_ADD(DATE(2015,5,3), INTERVAL CAST(FLOOR(DATE_DIFF(CAST(post_created_date_CT AS DATE), DATE('2015-05-03'), DAY)/7) AS INT64) WEEK) AS Week
+FROM `USdata.politician_us_user_post_like_all`
+WHERE TIMESTAMP(post_created_date_CT) >= TIMESTAMP('2015-05-03')
+AND TIMESTAMP(post_created_date_CT) < TIMESTAMP('2016-11-27'))
+)
+GROUP BY user_id, Week)
+WHERE week_total > 0)
+GROUP BY user_id)
